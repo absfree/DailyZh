c652003e8229fb26ee9ef5edc2cefb4f9c2b2746
absfree <xiaoyaoyou818@outlook.com>
2016-09-05 19:22:58 +0800

    Delete ResponseHandler.java

--- app/src/main/java/com/yxy/zlp/dailyzh/util/httpUtil/ResponseHandler.java
+++
@@ -1,9 +0,0 @@
-package com.yxy.zlp.dailyzh.util.httpUtil;
-
-/**
- * Created by Administrator on 2016/6/2.
- */
-public interface ResponseHandler {
-    void onSuccess(byte[] result);
-    void onFailure();
-}
