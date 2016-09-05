14a3ae0932736adba454af87ae73c7f95a085c9f
absfree <xiaoyaoyou818@outlook.com>
2016-09-05 19:22:21 +0800

    Update and rename Constants.java to Constant.java

---
+++ app/src/main/java/com/yxy/zlp/dailyzh/util/Constant.java
@@ -0,0 +1,17 @@
+package com.yxy.zlp.dailyzh.util;
+
+/**
+ * Created by Administrator on 2016/3/24.
+ */
+public final class Constants {
+    public static final String BASE_URL = "http://news-at.zhihu.com/api/4/";
+    public static final String START_IMG = "start-image/480*728";
+    public static final String LATEST_NEWS = "news/latest";
+    public static final String PREV_NEWS = "news/before/";
+    public static final String NEWS_CONTENT = "news/";
+    public static final String THEME_LIST = "themes";
+    public static final String THEME = "theme/";
+
+    public static final int TOPIC = 818;
+
+}
