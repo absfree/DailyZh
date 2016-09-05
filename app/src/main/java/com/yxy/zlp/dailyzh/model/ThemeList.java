01178c473db5f875c7bad5763c5a54cb96347508
absfree <xiaoyaoyou818@outlook.com>
2016-09-05 19:16:27 +0800

    Create ThemeList.java

---
+++ app/src/main/java/com/yxy/zlp/dailyzh/model/ThemeList.java
@@ -0,0 +1,18 @@
+package com.yxy.zlp.dailyzh.model;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/9/4.
+ */
+public class ThemeList {
+    private List<Theme> others;
+
+    public List<Theme> getOthers() {
+        return others;
+    }
+
+    public void setOthers(List<Theme> others) {
+        this.others = others;
+    }
+}
